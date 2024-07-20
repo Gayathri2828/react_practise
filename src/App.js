8d7ebd81f639a30c198f9a00dde0c7cac61343a1
gayathri2828 <gayathrikunduru28@gmail.com>
2024-07-20 23:31:29 +0530

    hooks intro & useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
  import React from "react";
- import { Card } from "./components/card/card.js";
- import { ImageComponents } from "./components/image/imageComponent.js";
- import { ListParentOrdered } from "./components/list/listParent.js";
+// import { Card } from "./components/card/card.js";
+// import { ImageComponents } from "./components/image/imageComponent.js";
+// import { ListParentOrdered } from "./components/list/listParent.js";
  import "./index.css";
- import ButtonComponentClass from "./components/class-components/button.js";
-import TodoComponent from "./components/class-components/todo/todo.js";
-import ProductListing from "./components/class-components/productListing/productListing.js";
-import Weather from "./components/weather/weather.js";
+// import ButtonComponentClass from "./components/class-components/button.js";
 
-import Counter from "./components/task/count.js";
-import MountingPhase from "./components/class-components/mounting/mounting.js";
-import Unmounting from "./components/class-components/unmounting/unMounting.js";
-import ParentComponent from "./components/class-components/purecomponent/parent.js";
-import CircleComponent from "./components/class-components/circleTask/circles.js";
+//import ProductListing from "./components/class-components/productListing/productListing.js";
+//import Weather from "./components/weather/weather.js";
+
+//import Counter from "./components/task/count.js";
+//import MountingPhase from "./components/class-components/mounting/mounting.js";
+//import Unmounting from "./components/class-components/unmounting/unMounting.js";
+//import ParentComponent from "./components/class-components/purecomponent/parent.js";
+//import CircleComponent from "./components/class-components/circleTask/circles.js";
+//import UseStateExample from "./components/functionalHooks/useState/useState.js";
+
+import TodoComponents from "./components/functionalHooks/useState/todos.js";
 
 
 const App = () => {
-const isLogin = true;
+//const isLogin = true;
   return (
      <div>
-       {/*<MountingPhase favColor={"red"}/>*/}
-       {/*<Unmounting/>*/}
-       {/*<ParentComponent/>*/}
-       <CircleComponent/>
-       
+      
+      <TodoComponents/>
         
       </div>
     );
